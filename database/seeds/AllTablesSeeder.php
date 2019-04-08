76ed15b6efd8f7aa901b64ca91e70beeadb81f45
wnbsmart <mmacola@tvz.hr>
2019-04-08 09:05:19 +0200

    Seeder updated

--- database/seeds/AllTablesSeeder.php
+++ database/seeds/AllTablesSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use \Illuminate\Support\Str;
 
-class ContactsTableSeeder extends Seeder
+class AllTablesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,11 +13,18 @@ class ContactsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => '$2y$10$S8ZOu44JQg6oROx3fCw/cuYt2UDjNgvyS52gRRtKMytaePl.2lery',
+        ]);
+
         DB::table('contacts')->insert([
             'user_id' => 1,
             'name' => Str::random(10),
             'surname' => Str::random(10),
-            'image' => 'image.jpg',
+            'image' => '5caa47dc670f8.jpg',
             'email' => Str::random(10).'@gmail.com',
             'favourite' => 0,
         ]);
@@ -26,9 +33,21 @@ class ContactsTableSeeder extends Seeder
             'user_id' => 1,
             'name' => Str::random(10),
             'surname' => Str::random(10),
-            'image' => 'image.jpg',
+            'image' => '5caa47dc670f8.jpg',
             'email' => Str::random(10).'@gmail.com',
             'favourite' => 1,
+        ]);
+
+        DB::table('phones')->insert([
+            'contact_id' => 1,
+            'number' => '123123',
+            'label' => 'work'
+        ]);
+
+        DB::table('phones')->insert([
+            'contact_id' => 1,
+            'number' => '5234',
+            'label' => 'home'
         ]);
     }
 }
